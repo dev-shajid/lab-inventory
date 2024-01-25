39bd545d0c3fbd42c9632684f8b6d879b59ef06e
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2024-01-26 02:13:00 +0600

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@
 module.exports = {
   darkMode: ["light"],
   content: [
+    "node_modules/@mantine/core/styles.css",
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "node_modules/@mantine/core/styles.css",
   ],
   theme: {
     container: {
