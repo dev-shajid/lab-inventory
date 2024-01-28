53fbebdc179b5f0c4cbf3c0731e589e88eb7f91c
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2024-01-28 07:47:23 +0600

    add,edit,delete item and user added

--- src/app/api/user/roleLabManager/route.js
+++ src/app/api/user/roleLabManager/route.js
@@ -7,7 +7,7 @@ export const POST = async (req) => {
         await db.connect()
         const id=await req.json()
         console.log({id})
-        const user = await User.findOneAndUpdate({ _id:id }, {role: 'manager'})
+        const user = await User.findOneAndUpdate({ _id:id }, {role: 'manager', lab: null})
         return new Response(JSON.stringify(user), { status: 200 })
     } catch (error) {
         return new Response(JSON.stringify(null), { status: 500 })
